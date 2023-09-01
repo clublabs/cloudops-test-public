98c4d6d5692311f45acd0980df969801e95dbb7e
Khoa Nguyen <42684216+aaa-khoa@users.noreply.github.com>
2023-09-01 10:06:53 -0700

    feat: add stuff to app stack

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,16 +1,35 @@
-import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
-export class AppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+export class AppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'AppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new sqs.Queue(this, 'AppQueue', {
+      visibilityTimeout: Duration.seconds(300)
+    });
+
+     // Create a Lambda function
+     const helloWorldLambda = new Function(this, 'HelloWorldLambda', {
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: Code.fromInline(`
+        exports.handler = async (event) => {
+          const response = {
+            statusCode: 200,
+            body: JSON.stringify('Hello, World!'),
+          };
+          return response;
+        };
+      `),
+    });
+
+
+
+
   }
 }
